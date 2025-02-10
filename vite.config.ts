dd802053665f4b8584f46f21e93717c76125c471
João Victor Porto <140113109+Jportov@users.noreply.github.com>
2025-02-09 23:58:57 -0300

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Garante que o Vercel sirva corretamente os arquivos
   test: {
     setupFiles: "./src/setupTests.ts",
     globals: true,
